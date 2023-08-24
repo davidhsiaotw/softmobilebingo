0f91b376f8baf2939e77216e167f6fd20440b547
davidhsiaotw <wyxiao6@gmail.com>
2023-08-24 23:45:25 +0800

    feat: show bingo dialog

--- app/src/main/java/com/example/bingo/MainActivity.kt
+++ app/src/main/java/com/example/bingo/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.bingo
 
+import android.app.ProgressDialog.show
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -19,10 +21,10 @@ class MainActivity : AppCompatActivity() {
         val bingoViewModel = BingoViewModel()
         val bingoRecyclerView = findViewById<RecyclerView>(R.id.grids_recylerview)
 
-//        Log.d("MainActivity", "${bingoViewModel.bingo.value?.size}")
         val startButton = findViewById<Button>(R.id.start_button)
         startButton.setOnClickListener {
             bingoViewModel.startBingo()
+            (bingoRecyclerView.adapter as BingoAdapter).submitList(bingoViewModel.bingo.value)
         }
 
         val disableScrollGridLayoutManager = object : GridLayoutManager(this, 3) {
@@ -32,14 +34,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         bingoRecyclerView.layoutManager = disableScrollGridLayoutManager
-
         // create bingo adapter
         bingoRecyclerView.adapter = BingoAdapter(GridListener {
             it.isSelected = !it.isSelected
             if (bingoViewModel.checkBingo()) {
-                Toast.makeText(this, "BINGO!!", Toast.LENGTH_SHORT).show()
                 // show bingo dialog
-                val dialog = MaterialAlertDialogBuilder(this)
+                MaterialAlertDialogBuilder(this).setMessage("BINGO!!")
+                    .setNeutralButton("RESTART") { _, _ ->
+                        bingoViewModel.startBingo()
+                        (bingoRecyclerView.adapter as BingoAdapter).submitList(bingoViewModel.bingo.value)
+                    }.show()
             }
         })
         (bingoRecyclerView.adapter as BingoAdapter).submitList(bingoViewModel.bingo.value)
